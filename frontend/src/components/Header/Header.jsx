c0a4ad4b95a92035e70d7ec72e1811a358399508
Anshika Singh <162441026+Ansh1ka15@users.noreply.github.com>
2026-01-27 01:26:42 +0530

    Menu,Navbar,Header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingreadients and culmary expertise. Our missionis to satisfy your cravings and elevate your dining expereince, one delicious meal at time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+export default Header
